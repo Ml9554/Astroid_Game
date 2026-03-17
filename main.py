1472721c5264c50904e397840c11ea5dc3152373
ml9554 <matt_lee_95@hotmail.co.uk>
2026-03-17 08:11:35 +0000

    added fps limiter

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
     
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +21,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
 
     
 
